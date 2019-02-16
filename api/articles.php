ecd9bd5c635fa457b952437849fda5d1ad5f5931
Stevie Comyn <phoneageekdundee@gmail.com>
2019-02-16 17:01:35 +0000

    Improved Comments, added switch case for POST Methods.

--- api/articles.php
+++ api/articles.php
@@ -1,30 +1,39 @@
 <?php
 
+// Pulls Database connection and Functions.
 include('../config/connection.php');
 
+// Determine the type of Request being recieved.
 $request_method=$_SERVER["REQUEST_METHOD"];
 
-// For Testing Purposes.
-// echo $request_method;
+// filters response based on Request Type.
+switch($request_method){
+	
+	case 'GET':
+		// Retrive specific Artile by Id.
+		if(!empty($_GET["id"])){
 
-switch($request_method)
-	{
-		case 'GET':
-			// Retrive Products
-			if(!empty($_GET["id"]))
-			{
-				$id=intval($_GET["id"]);
-				oneArticleJSON($id);
-			}
-			else
-			{
-				allArticlesJSON();
-			}
-			break;
-		default:
-			// Invalid Request Method
-			header("HTTP/1.0 405 Method Not Allowed");
-			break;
+			$id=intval($_GET["id"]);
+			oneArticleJSON($id);
+
+		} else {
+			// Show all Articles.
+			allArticlesJSON();
+		}
+		break;
+
+	case 'POST':
+
+		if(!empty($_POST['something'])){
+
+			//Do something.
+		}
+		break;
+
+	default:
+		// Invalid Request Method
+		header("HTTP/1.0 405 Method Not Allowed");
+		break;
 	}
 
 ?>
