4f2d93c23d8836f1137aba02585efb62492463cd
Stevie Comyn <phoneageekdundee@gmail.com>
2019-02-16 19:14:22 +0000

    added markup for view blog post test interface.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require('config/connection.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>JSON API Backend</title>
 </head>
 <body>
@@ -27,6 +28,44 @@ require('config/connection.php');
     <br><br>
     <input type="submit" value="Submit">
 </form>
+
+<br><hr>
+
+<h2>View a Blog Post</h2>
+
+<ul>
+    <li><a href="">Week 1</a></li>
+    <li><a href="">Week 2</a></li>
+    <li><a href="">Week 3</a></li>
+    <li><a href="">Week 4</a></li>
+    <li><a href="">Week 5</a></li>
+</ul>
+
+<p>Content Div</p>
+<div class="content" style="border: 1px solid #000; padding: 1em; width: 60%;">
+</div>
+
+<script>
+
+    $(function(){
+
+        var $content = $('.content');
+
+        $.ajax({
+            type: 'GET',
+            dataType: "json",
+            url: '/blogApi/api/posts/',
+            success: function(posts){
+                $.each(posts, function(i, item) {
+                    $content.append('<span>'+ item.articleId + ' - ' + item.title +'</span><br>');
+                });
+            }
+        });
+    });
+
+
+
+</script>
 
 <br><hr>
 <p>Stevie Comyn &copy; 2019</p>
