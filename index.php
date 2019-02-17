a011662ddf3a076401e84da051b77e77175e516d
Stevie Comyn <phoneageekdundee@gmail.com>
2019-02-17 23:01:34 +0000

    added Favicon.ico link..

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require('config/connection.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="favicon.ico">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>JSON API Backend</title>
     <style>
